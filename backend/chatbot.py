ff3c25d13cc61278356bc42b258c9952fed763c7
Shang-Ching Liu <shangching@live.com>
2023-06-08 07:22:23 +0000

    update chatbot

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -203,9 +203,13 @@ class ChatBot():
                 "---------------------\n"
                 "{context_str}"
                 "\n---------------------\n"
-                "If the Context information is empty, than try to answer the question: {query_str}."
-                "If the Context information in not empty, given the context information and not prior knowledge, "
-                "answer the question: {query_str}\n"
+
+                "Context information is below. \n"
+                "---------------------\n"
+                "{query_str}"
+                "\n---------------------\n"
+                "If the Context information is empty, than try to answer the question without consider context information."
+                "If the Context information in not empty, given the context information and not prior knowledge.\n"
             )
             prompt = QuestionAnswerPrompt(qa_promtm_tmpl)
             response_synthesizer = ResponseSynthesizer.from_args(
